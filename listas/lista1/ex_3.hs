91a24b8ebe850646e3f460cd2a12c71d6b6a8cd6
Thiago Martins <thiago.martins1112@gmail.com>
2025-03-22 02:02:36 -0300

    chore: indica erro na função multiplica a ser resolvido posteriormente

--- listas/lista1/ex_3.hs
+++ listas/lista1/ex_3.hs
@@ -1,6 +1,7 @@
 soma :: Int -> Int -> Int
 soma x y = x + y
 
+--TODO: Consertar erro na lógica, primeira recorrência é incorreta, estado inicial de x não é salvo--
 multiplica :: Int -> Int -> Int
 multiplica x 0 = 0
 multiplica x 1 = x
