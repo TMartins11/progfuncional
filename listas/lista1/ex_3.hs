eab08ef92a043ea3671ec30cd0deb71958857537
Thiago Martins <thiago.martins1112@gmail.com>
2025-06-05 01:27:16 -0300

    chore: reorganizando repositório.

--- listas/lista1/ex_3.hs
+++
@@ -1,6 +0,0 @@
-soma :: Int -> Int -> Int
-soma x y = x + y
-
-multiplica :: Int -> Int -> Int
-multiplica x 0 = 0
-multiplica x y = soma x (multiplica x (y-1))
--- listas/lista1/ex_5.hs
+++
@@ -1,5 +0,0 @@
-square :: Int -> Int
-square x = x * x
-
-fourPower :: Int -> Int
-fourPower x = square (square x)
