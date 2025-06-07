3b02d4ea5c2b2a17ca6c212be2a9416e17feb810
Thiago Martins <thiago.martins1112@gmail.com>
2025-06-07 16:08:56 -0300

    chore: reorganizando rep.

--- listas/L1/ex_10.hs
+++
@@ -1,6 +0,0 @@
-{-Escreva, em Haskell, uma função que retorna o último dígito de um número inteiro.-}
-
-lastDigit :: Int -> Int
-lastDigit x
-    | x >= 0 =  x `mod` 10
-    | otherwise = (-x) `mod`  10
